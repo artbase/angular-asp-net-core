726a3cad17a1c389005162d303f574dfc3587ea4
Bartosz Zaremba <bartosz.zaremba@live.com>
2017-10-22 11:46:12 +0200

    Added a few loaders for css, files and fonts

--- AngularAndAspNetCore/app/config/webpack.config.js
+++ AngularAndAspNetCore/app/config/webpack.config.js
@@ -5,5 +5,32 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    publicPath: 'dist/'
+                }
+            }]
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [{
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: 'dist/'
+                    }
+            }]
+        }]
     }
 };
--- AngularAndAspNetCore/app/root.js
+++ AngularAndAspNetCore/app/root.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
 
+import './src/main/style.css';
+
+import icon from './src/img/cat.png';
+
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = icon;
+  element.appendChild(myIcon);
+  
   return element;
 }
 
