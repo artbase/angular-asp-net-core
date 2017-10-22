d68b5a628ae70c3d676972373be9a927a11e9a90
Bartosz Zaremba <bartosz.zaremba@live.com>
2017-10-22 14:06:44 +0200

    Added devtool for source map and set a few scripts for watching files and start local dev server

--- AngularAndAspNetCore/app/webpack.config.js
+++ AngularAndAspNetCore/app/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
